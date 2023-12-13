faffbfe20bd2b556a2056b3596ae117645848a49
Ivan Suarez <ivan.suarez.fabara@gmail.com>
2023-12-13 13:25:27 -0500

    Revert "index_service creation"
    This reverts commit 6ef5d63b43769b250344abf9361f7f8f3a8c18cb.

--- index_service/views.py
+++ index_service/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render
-import os
-import ast
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-def index(request):
-    options = ast.literal_eval(os.getenv("OPTIONS", "[]"))
-    return render(request, 'index.html', {'options': options})
-
+# Create your views here.
